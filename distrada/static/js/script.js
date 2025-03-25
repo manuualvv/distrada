b9e244768d9f12cdbfde535cadc67e287e7024b9
mmaranetto <alvarezmaranetto@gmail.com>
2025-03-25 01:07:41 -0300

    'nada'

--- distrada/static/js/script.js
+++ distrada/static/js/script.js
@@ -29,16 +29,17 @@ var swiper = new Swiper(".slide-content", {
   },
 });
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-    // } else {
-    //   entry.target.classList.remove("show");
-    // }
-  });
-});
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+// NO SÉ SI UTILIZARLO DE MOMENTO
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("show");
+//     }
+//     // } else {
+//     //   entry.target.classList.remove("show");
+//     // }
+//   });
+// });
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el));
