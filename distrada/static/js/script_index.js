f949c8012b5b6ee04eb7944ce9fa7a37bfea65a4
mmaranetto <alvarezmaranetto@gmail.com>
2025-04-10 12:11:07 -0300

    'styleChanger'

--- distrada/static/js/script_index.js
+++ distrada/static/js/script_index.js
@@ -45,17 +45,51 @@ var swiper = new Swiper(".slide-content", {
 // hiddenElements.forEach((el) => observer.observe(el));
 
 document.addEventListener("DOMContentLoaded", function () {
-  // SELECCIÓN DE ITEMS
+  // ||| MODAL_SLIDER ||| SELECCIÓN DE ITEMS
   const productCard = document.querySelector(".product_card");
   const productCloseBtn = document.querySelector(".product_closebtn");
   const container_products = document.querySelectorAll(".card");
   const overlay = document.querySelector(".overlay");
 
-  // ITERACIÓN EN CADA PRODUCTO
+  // ||| MODAL_SLIDER ||| ITERACIÓN EN CADA PRODUCTO
   container_products.forEach((card) => {
     card.addEventListener("click", function () {
       productCard.classList.toggle("hidden");
       overlay.classList.toggle("hidden");
+    });
+  });
+
+  // ||| MODAL_SLIDER ||| CERRAR CON BOTON X
+  productCloseBtn.addEventListener("click", function () {
+    productCard.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+  });
+
+  // ||| CHANGE_STYLE ||| SELECCIÓN DE ITEMS
+  const changeStyleBtns = document.querySelectorAll(".change-style-btn");
+  const heroSection = document.querySelector(".section-hero"); // PROBANDO
+  const heroText1 = document.querySelector(".hero-text"); // PROBANDO
+  const heroImg = document.querySelector(".hero-img"); // PROBANDO
+
+  // ||| CHANGE_STYLE ||| ON CLICK
+  let actualStyle = 0;
+  changeStyleBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      if (actualStyle === 0) {
+        heroSection.style.backgroundImage =
+          "linear-gradient( #f1f1f1, rgb(107, 30, 30))";
+        heroText1.textContent = "Una nueva colección está llegando a Strada"; // PROBANDO
+        heroImg.src = heroImg.dataset.lucas;
+        actualStyle = 1;
+      } else {
+        heroSection.style.backgroundImage =
+          "linear-gradient(rgb(255, 255, 255), #f1f1f1)";
+        heroText1.textContent = "Tenemos una nueva colección para ustedes"; // PROBANDO
+        heroImg.src = "{% static 'img/hero-martos.png' %}";
+        heroImg.src = heroImg.dataset.martos;
+        actualStyle = 0;
+      }
+      // PARA CADA COLOR (ESTILO), CREAR UNA CLASE Y JUGAR CON LOS ESTILOS
     });
   });
 });
